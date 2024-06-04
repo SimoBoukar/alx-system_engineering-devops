05921f5ee4b76382215d5aec4153c67862b096ba
Mohammed <6bouass9@gmail.com>
2024-06-05 00:27:43 +0100

    push mandatory tasks v2.0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,19 @@
 """
 Contains the number_of_subscribers function
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """ returns the number of subscribers for a given subreddit. """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json().get('data')
+        subs = data.get('subscribers')
+        return subs
+    except (KeyError, requests.RequestException):
         return 0
-    r_sub = requests.get(
-            'http://www.reddit.com/r/{}/about.json'.format(subreddit),
-            headers={'User-Agent': '0x16-api_advanced:project:\
-                v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r_sub.get("data", {}).get("subscribers", 0)
-    return subs
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,15 +7,14 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-        v1.0.0 (by /u/firdaus_cartoon_jr)"
-        }
-    param = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
         "limit": 10
-        }
+    }
     response = requests.get(
             url, headers=headers,
-            param=param, allow_redirects=False)
+            params=params, allow_redirects=False)
     if response.status_code == 404:
         print("None")
         return
