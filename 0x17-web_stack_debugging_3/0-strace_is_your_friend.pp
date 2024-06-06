99952f6357e06d17c8b3cc80082eb5a5b9de0c78
Mohammed <6bouass9@gmail.com>
2024-06-06 01:19:57 +0100

    push task v2.0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-exec {'replaces wrong php filetype':
-  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /var/www/html/wp-settings.php'
-  }
+# Using strace, find out why Apache is returning a 500 error.
+
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
